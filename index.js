353760500a3a0989c5dfdabe06b639c5669c96c3
Zalak Patel <zalak.patel@solutionanalysts.com>
2017-01-16 15:57:07 +0530

    uncommented code

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
         var express = require("express");
         var app = express();
-        // const crypto = require('crypto');
+        const crypto = require('crypto');
         var bodyParser = require('body-parser');
-        // var mongoose = require('mongoose');
-        // var User = require('./Models/user')
-        // mongoose.connect('mongodb://localhost/EMS');
+        var mongoose = require('mongoose');
+        var User = require('./Models/user')
+        mongoose.connect('mongodb://localhost/EMS');
 
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
 
         // // Register
 
-        // app.post('/adduser', function(req,res){
+        app.post('/adduser', function(req,res){
             
-        // if(req.body.firstname.length > 3)
-        //     {
-        //           User.find({email: req.body.email},function(err,users){
-        //   if(err) throw err;
-        //   if(users.length){
-        //        res.send("User already exist")
-        //   }else{
+        if(req.body.firstname.length > 1)
+            {
+                  User.find({email: req.body.email},function(err,users){
+          if(err) throw err;
+          if(users.length){
+               res.send("User already exist")
+          }else{
               
-        //       const pass = crypto.createHmac('sha256', req.body.password)
-        //            .update('d')
-        //            .digest('hex');
-        //       console.log(pass);
-        //       req.body.password = pass
-        //         User.create(req.body,function (err,user){
+              const pass = crypto.createHmac('sha256', req.body.password)
+                   .update('d')
+                   .digest('hex');
+              console.log(pass);
+              req.body.password = pass
+                User.create(req.body,function (err,user){
 
-        //         if(err) {
-        //             res.send("Some error occured during the creation" + err);
-        //         } else {
-        //             res.status(200);
-        //             res.send(" New User added " + user.firstname  + " " +  user.lastname + "\n "  + user.email + " " + user.password);
-        //         }
-        //     });
-        //   }
-        //   });
-        //     }else{
-        //         res.send("Please enter atleast 3 characters for first name" );
-        //     }
+                if(err) {
+                    res.send("Some error occured during the creation" + err);
+                } else {
+                    res.status(200);
+                    res.send(" New User added " + user.firstname  + " " +  user.lastname + "\n "  + user.email);
+                }
+            });
+          }
+          });
+            }else{
+                res.send("Please enter atleast 2 characters for first name" );
+            }
 
-        // });
+        });
 
-        // //Login
+        //Login
 
-        // app.post('/login', function(req,res){
+        app.post('/login', function(req,res){
 
-        //   User.find({email : req.body.email , password : req.body.password}, function(err, users){
+          User.find({email : req.body.email , password : req.body.password}, function(err, users){
 
-        //     if(err) throw err;
+            if(err) throw err;
 
-        //     if(users.length == 1){
-        //         console.log(users)
-        //         res.send("User logged in with name:- " + users[0].firstname)
-        //     }
-        // else{
-        //      res.send("Invalid data")
-        // }
-        // });
-        // });
+            if(users.length == 1){
+                console.log(users)
+                res.send("User logged in with name:- " + users[0].firstname)
+            }
+        else{
+             res.send("There is no user with given email or password")
+        }
+        });
+        });
 
 
-        // app.listen(3000, function () {
-        //   console.log('Example app listening on port 3000!');
-        // });
+        app.listen(3000, function () {
+          console.log('Example app listening on port 3000!');
+        });
 
 //Example
 //get method
@@ -92,7 +92,7 @@
 //   res.send(req.body)
 // })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
 
